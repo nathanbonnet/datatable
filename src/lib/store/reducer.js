8725dc809eb96074562a6373b2eee5eec1eeddb4
Nathan <nathbonnet@hotmail.fr>
2023-01-09 15:53:51 +0100

    change reducer

--- src/lib/store/reducer.js
+++ src/lib/store/reducer.js
@@ -1,51 +1,51 @@
 const initialState = {
-    page: 1,
-    result: 10,
-    sources: [{firstName: 'toto'}, {firstName: 'tata'}],
-    dataOrigine: [{firstName: 'test'}, {firstName: 'titi'}],
-    dataNumber: 10,
-    order: "asc",
-    name: "firstName",
+  page: 1,
+  result: 10,
+  sources: [],
+  dataOrigine: [],
+  dataNumber: 10,
+  order: "asc",
+  name: "firstName",
 }
 
 
 export const reducer = (state = initialState, action) => {
-    if (action.type === "PAGE") {
-      return {
-        ...state,
-        page: state.page = action.page
-      };
-    }
-    if (action.type === "RESULT") {
-      return {
-        ...state,
-        result: state.result = action.result
-      };
-    }
-    if (action.type === "BODY") {
-      return {
-        ...state,
-        sources: state.sources = action.sources
-      };
-    }
-    if (action.type === "DATA_ORIGINE") {
-      return {
-        ...state,
-        dataOrigine: state.dataOrigine = action.dataOrigine
-      };
-    }
-    if (action.type === "DATA_NUMBER") {
-      return {
-        ...state,
-        dataNumber: state.dataNumber = action.dataNumber
-      };
-    }
-    if (action.type === "ORDER") {
-      return {
-        ...state,
-        order: state.order = action.order,
-        name: state.name = action.name
-      };
-    }
-    return state
+  if (action.type === "PAGE") {
+    return {
+      ...state,
+      page: state.page = action.page
+    };
+  }
+  if (action.type === "RESULT") {
+    return {
+      ...state,
+      result: state.result = action.result
+    };
+  }
+  if (action.type === "BODY") {
+    return {
+      ...state,
+      sources: state.sources = action.sources
+    };
+  }
+  if (action.type === "DATA_ORIGINE") {
+    return {
+      ...state,
+      dataOrigine: state.dataOrigine = action.dataOrigine
+    };
+  }
+  if (action.type === "DATA_NUMBER") {
+    return {
+      ...state,
+      dataNumber: state.dataNumber = action.dataNumber
+    };
+  }
+  if (action.type === "ORDER") {
+    return {
+      ...state,
+      order: state.order = action.order,
+      name: state.name = action.name
+    };
+  }
+  return state
 }
